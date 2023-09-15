871a5ec0b5b3b9259b6f8f112df8c701f8d149d1
hlibyelkhov <hlib.yelkhov@gmail.com>
2023-09-15 11:55:49 +0200

    Starter commits

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,15 @@ function Todolist() {
     return (
         <div>
             <h3>What to learn</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={true}/><span>CSS&HTML</span></li>
+                <li><input type="checkbox" checked={true}/><span>JS</span></li>
+                <li><input type="checkbox" checked={false}/><span>React</span></li>
+            </ul>
         </div>
     )
 }
