5b35e471a4469e7d191a4e4f79e30e9d2922cb2a
hlibyelkhov <hlib.yelkhov@gmail.com>
2023-09-15 12:03:21 +0200

    Main structure of my App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
 
@@ -11,29 +12,6 @@ function App() {
             <Todolist/>
         </div>
     );
-}
-
-function Todolist() {
-
-    return (
-        <div>
-            <h3>What to learn</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                <li><input type="checkbox" checked={true}/><span>CSS&HTML</span></li>
-                <li><input type="checkbox" checked={true}/><span>JS</span></li>
-                <li><input type="checkbox" checked={false}/><span>React</span></li>
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>
-        </div>
-    )
 }
 
 export default App;
