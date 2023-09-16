2aea346b610b04ef0afb3bb393e922f55e2fd8c9
hlibyelkhov <hlib.yelkhov@gmail.com>
2023-09-16 19:33:00 +0200

    Props and props type

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,22 @@ import {Todolist} from "./Todolist";
 
 function App() {
 
+    let tasks1 = [
+        { id: 1, title: "CSS", isDone: true },
+        { id: 1, title: "JS", isDone: true },
+        { id: 1, title: "React", isDone: false }
+    ]
+
+    let tasks2 = [
+        { id: 1, title: "Terminator", isDone: true },
+        { id: 1, title: "XXX", isDone: false },
+        { id: 1, title: "Gentelments of fortuna", isDone: true }
+    ]
+
     return (
         <div className="App">
-
             <Todolist title={"What to learn"}/>
             <Todolist title={"Movies"}/>
-            <Todolist title={"Songs"}/>
-
-            <input type="checkbox"/>
-            <input type="date"/>
-            <input placeholder={"it-incubator"}/>
         </div>
     );
 }
