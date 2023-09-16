a850695dd28cd8419cb6ad5d98b1f0bcb3df4b31
hlibyelkhov <hlib.yelkhov@gmail.com>
2023-09-16 19:48:03 +0200

    Props and props type

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -27,9 +27,9 @@ export function Todolist(props: PropsTitle) { // props =  { title: "What to lear
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={true}/><span>CSS&HTML</span></li>
-                <li><input type="checkbox" checked={true}/><span>JS</span></li>
-                <li><input type="checkbox" checked={false}/><span>React</span></li>
+                <li><input type="checkbox" checked={props.tasks[0].isDone}/><span>{props.tasks[0].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[1].isDone}/><span>{props.tasks[1].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[2].isDone}/><span>{props.tasks[2].title}</span></li>
             </ul>
             <div>
                 <button>All</button>
